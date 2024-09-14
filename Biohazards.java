53bee9e1fb69d0e001b206c77868b9acde07d4d3
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-09-14 10:21:32 +0530

    Update Biohazards.java

--- Biohazards.java
+++ Biohazards.java
@@ -4,56 +4,61 @@ import java.util.HashSet;
 import java.util.List;
 
 class Result {
-    private Result(){}
+    private Result() {}
     
-
     public static long bioHazard(int n, List<Integer> allergic, List<Integer> poisonous) {
+      
         HashMap<Integer, HashSet<Integer>> hashMap = new HashMap<>();
-        
-        int index = 0;
 
-        HashSet<Integer> set = null;
+        for (int i = 0; i < allergic.size(); i++) {
+            int poison = poisonous.get(i);
+            int allergen = allergic.get(i);
+            hashMap.putIfAbsent(poison, new HashSet<>());
+            hashMap.get(poison).add(allergen);
+        }
 
-        for(int poison: poisonous) {
-            if(hashMap.containsKey(poison)){
-                hashMap.get(poison).add(allergic.get(index));
-            }else {
-                set = new HashSet<>();
-                set.add(allergic.get(index));
-                hashMap.put(poison, set);
+        long validIntervals = 0;
+        for (int start = 1; start <= n; start++) {
+            for (int end = start; end <= n; end++) {
+                if (isValidInterval(start, end, hashMap)) {
+                    validIntervals++;
+                }
             }
-            index+=1;
         }
-        int intervals = 0;
-        List<Integer> sequence = new ArrayList<>();
-        for(int i=1;i<=n;i++) {
-            if(hashMap.containsKey(i) && hashMap.get(i).contains(i+1)){
-                continue;
-            }
-            if(hashMap.containsKey(i+1) && hashMap.get(i+1).contains(i)){
-                continue;
-            }
-            sequence.add(i);
-        }
-        int size = sequence.size();
-        long result =(long)Math.pow(2, size) - 1 - size;
-        System.out.println(result + n);
-        return intervals;
+
+        return validIntervals;
     }
 
+    private static boolean isValidInterval(int start, int end, HashMap<Integer, HashSet<Integer>> hashMap) {
+    
+        for (int i = start; i <= end; i++) {
+            for (int j = i + 1; j <= end; j++) {
+                
+                if (hashMap.containsKey(i) && hashMap.get(i).contains(j)) {
+                    return false;
+                }
+                if (hashMap.containsKey(j) && hashMap.get(j).contains(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
 
-public class Solution {
+public class Biohazards {
     public static void main(String[] args) {
         List<Integer> a1 = new ArrayList<>();
         a1.add(2);
         a1.add(1);
         a1.add(3);
+        
         List<Integer> a2 = new ArrayList<>();
         a2.add(3);
         a2.add(3);
         a2.add(1);
-        long result = Result.bioHazard(3,a1,a2);
-        System.out.print(result);
+        
+        long result = Result.bioHazard(3, a1, a2);
+        System.out.println(result);  // Expected output: 4
     }
 }
